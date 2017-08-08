7ada336a14805b92fd09decb245d10d54ee31202
Joseph Marques <josephmarques@josephmarques.nyc.corp.google.com>
2017-08-08 09:29:43 -0400

    Forgot to pass in the service account credentials.

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -6,8 +6,9 @@ USAGE="$0 <project name> <cluster name> <bucket name>"
 PROJECT_NAME=${1:?Please provide a project name: $USAGE}
 CLUSTER_NAME=${2:?Please specify the name of the cluster: $USAGE}
 BUCKET_NAME=${3:?Please specify the name of the bucket where you want files saved: $USAGE}
+GCLOUD_SERVICE_KEY=${4:?Please enter the base64 encoded json keyfile: $USAGE}
 
-echo $GCLOUD_SERVICE_KEY_PRD | base64 --decode -i > ${HOME}/gcloud-service-key.json
+echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
 ./travis/build_and_push_container.sh gcr.io/${PROJECT_NAME}/downloader:$TRAVIS_COMMIT PROJECT_NAME
