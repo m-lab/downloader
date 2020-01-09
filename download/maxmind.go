6c6058e23bb1b2b06bda015651a856bf4e482b98
yachang <changya@gmail.com>
2020-01-09 13:36:25 -0500

    fix saved filename

--- download/maxmind.go
+++ download/maxmind.go
@@ -12,6 +12,16 @@ import (
 var maxmindURLToFilenameRegexp = regexp.MustCompile(`.*/()(.*)`)
 var maxmindFilenameToDedupeRegexp = regexp.MustCompile(`(.*/).*/.*`)
 
+var MaxmindDownloadInfo = []struct {
+	url      string
+	filename string
+}{
+	{
+		url:      "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN&suffix=tar.gz&license_key=",
+		filename: "GeoLite2-ASN_20200107.tar.gz",
+	},
+}
+
 // The list of URLs to download from Maxmind
 var MaxmindURLs []string = []string{
 	"https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN&suffix=tar.gz&license_key=",
@@ -29,11 +39,15 @@ var MaxmindURLs []string = []string{
 // files stored. It then downloads the files, stores them, and returns
 // and error on failure or nil on success. Gaurenteed to not introduce
 // duplicates.
-func DownloadMaxmindFiles(urls []string, timestamp string, store file.FileStore, maxmindLicenseKey string) error {
+func DownloadMaxmindFiles(timestamp string, store file.FileStore, maxmindLicenseKey string) error {
 	var lastErr error = nil
-	for _, url := range urls {
-		dc := DownloadConfig{URL: url + maxmindLicenseKey, Store: store, PathPrefix: "Maxmind/" + timestamp,
-			FilePrefix: time.Now().UTC().Format("20060102T150405Z-"), URLRegexp: maxmindURLToFilenameRegexp,
+	for index, _ := range MaxmindDownloadInfo {
+		dc := DownloadConfig{
+			URL:          MaxmindDownloadInfo[index].url + maxmindLicenseKey,
+			Store:        store,
+			PathPrefix:   "Maxmind/" + timestamp,
+			FilePrefix:   time.Now().UTC().Format("20060102T150405Z-"),
+			URLRegexp:    regexp.MustCompile(MaxmindDownloadInfo[index].filename),
 			DedupeRegexp: maxmindFilenameToDedupeRegexp}
 		if err := RunFunctionWithRetry(Download, dc, WaitAfterFirstDownloadFailure,
 			MaximumWaitBetweenDownloadAttempts); err != nil {
