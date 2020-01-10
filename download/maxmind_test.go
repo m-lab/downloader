b7ca493cce795da7b8ecd18257297271da580180
yachang <yachang@users.noreply.github.com>
2020-01-10 12:57:21 -0500

    Update maxmind_test.go

--- download/maxmind_test.go
+++ download/maxmind_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestAllMaxmindURLs(t *testing.T) {
-	for index, _ := range MaxmindDownloadInfo {
+	for index := range MaxmindDownloadInfo {
 		resp, err := http.Head(MaxmindDownloadInfo[index].url + os.Getenv("MAXMIND_LICENSE_KEY"))
 		if err != nil || resp.StatusCode != http.StatusOK {
 			t.Errorf("Bad URL (%q), err: %v (%v)", MaxmindDownloadInfo[index].url, err, resp.StatusCode)
