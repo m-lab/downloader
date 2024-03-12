359b8b758a8d39925a8a3d84ca5f8d512570504c
Nathan Kinkade <kinkade@measurementlab.net>
2024-03-12 12:11:47 -0600

    Updates maxmind_test.go to use HTTP Basic Auth

--- download/maxmind_test.go
+++ download/maxmind_test.go
@@ -12,15 +12,31 @@ import (
 // TODO: Distribute the configuration and code separately.
 func TestAllMaxmindURLs(t *testing.T) {
 	for index := range maxmindDownloadInfo {
+		user, found := os.LookupEnv("MAXMIND_ACCOUNT_ID")
+		if !found {
+			t.Error("Could not load Maxmind account ID from ${MAXMIND_ACCOUNT_ID}")
+			return
+		}
 		key, found := os.LookupEnv("MAXMIND_LICENSE_KEY")
 		if !found {
 			t.Error("Could not load Maxmind license key from ${MAXMIND_LICENSE_KEY}")
 			return
 		}
-		u := maxmindDownloadInfo[index].url + key
-		resp, err := http.Head(u)
+
+		url := maxmindDownloadInfo[index].url
+		req, err := http.NewRequest(http.MethodGet, url, nil)
+		if err != nil {
+			t.Errorf("http.NewRequest() failed for %q: %v ", url, err)
+		}
+
+		req.Close = true
+		req.SetBasicAuth(user, key)
+
+		client := http.Client{}
+		resp, err := client.Do(req)
+
 		if err != nil || resp.StatusCode != http.StatusOK {
-			t.Errorf("Bad URL (%q), err: %v (%v)", u, err, resp.StatusCode)
+			t.Errorf("Bad URL (%q), err: %v (%v)", url, err, resp.StatusCode)
 		}
 	}
 }
